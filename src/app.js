db86c7ba7cd98f2ee75b555f2dbdbbfee14cc04a
Dmm91 <moya91.dmm@gmail.com>
2024-11-18 21:34:16 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+
+        let emails = pronoun[i] + adj[j] + noun[k] + ".com"; // Creamos una variable que guarde el resultado de la iteración//
+
+        let emailsList = document.getElementById("emails"); //Creamos una variable sin definir y la asociamos al id de HTML//
+
+        let li = document.createElement("li"); // Creamos una variable que añade un elemento de lista //
+
+        emailsList.appendChild(li); //Declaramos la lista dentro de la variable emailsList //
+
+        li.innerHTML = emails; // Añadimos los resultados generados al elemento li dentro del HTML que a su vez se añade en emailsList que va asociado al id//
+      }
+    }
+  }
 };
